c2531e6dbc6beab5130edf3a86cc7cb2e6c5093e
Swoorupkisan <swoorupkisan3@.com>
2025-03-15 10:43:18 +0530

    Initial commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './home.css'
+import Header from '../../Components/Header/Header'
+import Exploremenu from '../../Components/ExploreMenu/Exploremenu'
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <Exploremenu/>
+    </div>
+  )
+}
+
+export default Home
