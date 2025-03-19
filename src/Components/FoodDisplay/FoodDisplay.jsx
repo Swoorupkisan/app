f929c18092911a617ccbad041af7943f5f949988
Swoorupkisan <swoorupkisan3@.com>
2025-03-19 23:55:53 +0530

    FoodItem and FoodDisplay components updated

--- src/Components/FoodDisplay/FoodDisplay.jsx
+++ src/Components/FoodDisplay/FoodDisplay.jsx
@@ -11,7 +11,7 @@ const {food_list} = useContext(StoreContext)
         <h2>Top dishes near you</h2>
         <div className="food-display-list">
           {food_list.map((item,index)=>{
-            return<FoodItem key={index} id={item.id} name={item.name} description={item.descreption} price={item.price} image={item.image}/>
+            return<FoodItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image}/>
           }) }
         </div>
 
--- src/Components/FoodItem/FoodItem.jsx
+++ src/Components/FoodItem/FoodItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
-const FoodItem = ({id,name,price,descreption,image}) => {
+const FoodItem = ({id,name,price,description,image}) => {
   return (
     <div>
       <div className='food-item'>
@@ -9,11 +9,11 @@ const FoodItem = ({id,name,price,descreption,image}) => {
             <img className='food-item-image' src={image} alt="" />
           </div>
           <div className='food-item-info'>
-            <div className="food-item-naem-rating">
+            <div className="food-item-name-rating">
               <p>{name}</p>
               <img src={assets.rating_starts} alt="" />
             </div>
-            <p className="food-item-desc">{descreption}</p>
+            <p className="food-item-desc">{description}</p>
             <p className="food-item-price">${price}</p>
 
           </div>
