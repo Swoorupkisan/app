637bbf21df54b91bad8a0497341e902e4aab0737
Swoorupkisan <swoorupkisan3@.com>
2025-03-21 11:33:48 +0530

    Footer and AppDownload components added

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -1,12 +1,34 @@
-import { createContext } from 'react'
-import { food_list } from '../assets/assets';
+import { createContext,useState,useEffect } from 'react'
+import {food_list} from '../assets/assets';
 
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
 
-const contextValue={
-    food_list
+    const [cartItems,setCartItems] = useState({});
+
+    const addToCart = (itemId) => {
+        if(!cartItems[itemId]){
+            setCartItems((prev)=>({...prev,[itemId]:1}))
+    }
+    else{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId] + 1}))
+    }
+}
+    const removeFromCart = (itemId) => {
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId] - 1}))
+    }
+
+    useEffect(() => {
+        console.log("Updated cart:", cartItems);
+      }, [cartItems]);
+
+const contextValue={                  //react context api
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart
 }
 return(
     <StoreContext.Provider value={contextValue}>
