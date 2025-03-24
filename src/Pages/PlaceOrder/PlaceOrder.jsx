5992f5ae7cc39e4ad9daf20e92fe4a4f3516a95a
Swoorupkisan <swoorupkisan3@.com>
2025-03-24 21:36:46 +0530

    Cart and PlaceOrder page updated

--- src/Pages/PlaceOrder/PlaceOrder.jsx
+++ src/Pages/PlaceOrder/PlaceOrder.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import './PlaceOrder.css'
 const PlaceOrder = () => {
   return (
-    <div>
+    <form className='place-order'>
+      <div className='place-order-left'>
+        <p className='title'>Delivery Information</p>
+        <div className='multi-fiels'>
+          <input type="text" placeholder='First name'/>
+          <input type="text" placeholder='Last name'/>
+        </div>
+        <input type="text" placeholder='Email address'/>
+        <input type="text" placeholder='Street'/>
+        <div className='multi-fiels'>
+          <input type="text" placeholder='City'/>
+          <input type="text" placeholder='State'/>
+        </div>
+        <div className='multi-fiels'>
+          <input type="text" placeholder='Zip code'/>
+          <input type="text" placeholder='Country'/>
+        </div>
+        <input type="text" placeholder='Phone' />
+
+      </div>
       
-    </div>
+    </form>
   )
 }
 
