fb7550e7b260b3a442b157ab7c55ffcf30f1f457
Swoorupkisan <swoorupkisan3@.com>
2025-03-25 20:15:11 +0530

    update cart and place order page

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -51,12 +51,12 @@ const Cart = () => {
             <hr />
             <div className='cart-total-details'>
               <p>Delivery Fee</p>
-              <p>₹{48}</p>
+              <p>₹{getTotalCartAmount()===0?0:48}</p>
             </div>
             <hr />
             <div className='cart-total-details'>
               <b>Total</b>
-              <b>₹{getTotalCartAmount()+48}</b>
+              <b>₹{getTotalCartAmount()===0?0:getTotalCartAmount()+48}</b>
             </div>
            </div>
             <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
