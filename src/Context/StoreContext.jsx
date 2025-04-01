d14aa2a2a4c1e32b142002e42990a8bea98dbed4
Swoorupkisan <swoorupkisan3@.com>
2025-04-02 00:57:52 +0530

    add to cart updated

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -21,8 +21,11 @@ const StoreContextProvider = (props) => {
             await axios.post(url +"/api/cart/add",{ itemId },{ headers:{token}})
         }
     }
-    const removeFromCart = (itemId) => {
-        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+    const removeFromCart = async (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+        if (token) {
+            await axios.post(url+"/api/cart/remove",{itemId},{headers:{token}})
+        }
     }
 
     const getTotalCartAmount = () => {
@@ -42,11 +45,17 @@ const StoreContextProvider = (props) => {
         setFoodList(response.data.data)
     }
 
+    const loadCartData = async (token) => {
+        const response = await axios.post(url+"/api/cart/get",{},{headers:{token}});
+        setCartItems(response.data.cartData);  
+    }
+
     useEffect(()=>{
         async function loadData() {
             await fetchFoodList();
             if (localStorage.getItem("token")) {
                 setToken(localStorage.getItem("token"));
+                await loadCartData(localStorage.getItem("token"));
             }   
         }
         loadData();
