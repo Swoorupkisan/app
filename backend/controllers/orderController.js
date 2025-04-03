ccb21a3f45ba11830ec0e623b1649b4cbc23fa99
Swoorupkisan <swoorupkisan3@.com>
2025-04-03 11:36:00 +0530

    admin changes

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,5 +7,25 @@ const placeOrder = async(req,res) =>{
 
 }
 
+const listOrders = async (req,res) =>{
+ try {
+    const orders = await orderModel.find({});
+    res.json({success:true,data:orders})
+ } catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"})
+ }
+}
 
-export {placeOrder}
+const updateStatus = async (req,res) => {
+  try {
+    await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status})
+    res.json({success:true,message:"Status Updated"})
+  } catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"})
+  }
+}
+
+
+export {placeOrder,listOrders,updateStatus}
